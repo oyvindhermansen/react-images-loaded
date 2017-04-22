badf34b50bd5d382bcb5aa8d65ee098362ef2b3a
oyvindhermansen <oyvind@ludensreklame.no>
2017-04-22 22:43:26 +0200

    working component

--- demo/demo.js
+++ demo/demo.js
@@ -3,14 +3,39 @@ import { render } from 'react-dom';
 import ImagesLoaded from '../src/index';
 
 class App extends Component {
+
+  handleOnAlways = (instance) => {
+    console.log('ALWAYS - all images have been loaded');
+  }
+
+  handleDone = () => {
+    console.log('DONE  - all images have been successfully loaded');
+  }
+
+  handleOnFail = () => {
+    console.log('FAIL - all images loaded, at least one is broken');
+  }
+
+  handleOnProgress = (instance, image) => {
+    const result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+  }
+
   render() {
     return (
       <div>
         This is demo 2
-        <ImagesLoaded>
-          <img src="" />
-          <img src="" />
-          <img src="" />
+        <ImagesLoaded
+          elementType={'div'}
+          onAlways={this.handleOnAlways}
+          done={this.handleDone}
+          onFail={this.handleOnFail}
+          onProgress={this.handleOnProgress}
+          className={'My-new-class'}
+        >
+          <img src="https://media.licdn.com/mpr/mpr/shrinknp_800_800/AAEAAQAAAAAAAAfUAAAAJGI4Mjg4ZDcwLWE1MDUtNDU2My04ODI5LTIzYTM1NzUxZDM5Yg.jpg" />
+          <img src="https://media.licdn.com/mpr/mpr/shrinknp_800_800/AAEAAQAAAAAAAAfUAAAAJGI4Mjg4ZDcwLWE1MDUtNDU2My04ODI5LTIzYTM1NzUxZDM5Yg.jpg" />
+          <img src="https://media.licdn.com/mpr/mpr/shrinknp_800_800/AAEAAQAAAAAAAAfUAAAAJGI4Mjg4ZDcwLWE1MDUtNDU2My04ODI5LTIzYTM1NzUxZDM5Yg.jpg" />
         </ImagesLoaded>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import imagesLoaded from 'imagesloaded';
+import omit from 'lodash.omit';
 
 export default class ImagesLoaded extends Component {
+  componentDidMount() {
+    // add events
+    const { onAlways, done, onFail, onProgress, background } = this.props;
+    const { elemContainer } = this.refs;
+    /* Initializing imagesLoaded */
+    this.imagesLoaded = imagesLoaded(elemContainer);
+
+    this.imagesLoaded.on('always', onAlways);
+    this.imagesLoaded.on('done', done);
+    this.imagesLoaded.on('fail', onFail);
+    this.imagesLoaded.on('progress', onProgress);
+  }
+
+  componentWillUnmount() {
+    const { onAlways, done, onFail, onProgress } = this.props;
+
+    this.imagesLoaded.off('always', onAlways);
+    this.imagesLoaded.off('done', done);
+    this.imagesLoaded.off('fail', onFail);
+    this.imagesLoaded.off('progress', onProgress);
+  }
+
   render() {
+    const { children } = this.props;
+    const props = omit(this.props, Object.keys(propTypes));
+
     return (
-      <div>
-        hello world 2
-      </div>
+      <this.props.elementType ref={'elemContainer'} {...props}>
+        {children}
+      </this.props.elementType>
     )
   }
+}
+
+ImagesLoaded.defaultProps = {
+  elementType: 'div',
+  className: 'images-loaded-container',
+  options: {}
+}
+
+const propTypes = {
+  loadingImages: PropTypes.func,
+  finishedLoading: PropTypes.func,
+  elementType: PropTypes.string,
+  onAlways: PropTypes.func,
+  done: PropTypes.func,
+  onFail: PropTypes.func,
+  onProgress: PropTypes.func,
+  options: PropTypes.object
 }
